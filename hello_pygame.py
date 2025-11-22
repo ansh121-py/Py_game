f2d411b4329347f113aaf38a8aeee38d32917b00
ansh121-py <mistryansh61@gmail.com>
2025-11-22 09:12:09 -0600

    Update hello_pygame.py

--- hello_pygame.py
+++ hello_pygame.py
@@ -1,1 +1,11 @@
+import pyame, sys
 
+pygame.init()
+Display = pygame.display.set_mode((400, 300))
+pygane.display.set_caption("Hello World!")
+while True:
+  for event in pygame.event.get():
+    if event.type == QUIT:
+      pygame.quit()
+      sys.exit()
+  pygame.display.update()
